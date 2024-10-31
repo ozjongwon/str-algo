9facaacfe618810fcb2cca34f8a9a0f45069e48a
jc <oz.jongwon.choi@gmail.com>
2024-10-31 17:05:44 +1100

    diff-operations

--- src/main.lisp
+++ src/main.lisp
@@ -71,29 +71,73 @@
                  (setf (aref k-vec (+ v-size i)) val)
                  (setf (aref k-vec i) val))))
       (loop initially (vset 1 0) ;; coord (0,0) = 0
-            for d from 0 below max-d do
-              (loop with x and y
-                    for k integer from (- d) to d by 2
-                    do (setf x (if (or (zerop (+ d k))
-                                       (and (/= k d)
-                                            (< (vget (1- k)) (vget (1+ k)))))
-                                   (vget (1+ k)) ;; use previous x
-                                   ;; new x, insert first
-                                   (1+ (vget (1- k))))
-                             y (- x k))
-                       (loop while (and (< x n)
-                                        (< y m)
-                                        (char= (aref str1 x)
-                                               (aref str2 y)))
-                             do (incf x)
-                                (incf y))
-                       (vset k x)
-                       (format t "~%*** (d k x y) = (~A ~A ~A ~A) k-vec: ~A"
-                               d k x y k-vec)
-                    when (and (>= x n)
-                              (>= y m))
-                      do (return-from myers-distance d))
+            with dkxy-history
+            for d from 0 below max-d
+            collect
+            (loop with x and y and dkxy-list
+                  for k integer from (- d) to d by 2
+                  do (setf x (if (or (zerop (+ d k))
+                                     (and (/= k d)
+                                          (< (vget (1- k)) (vget (1+ k)))))
+                                 (vget (1+ k)) ;; use previous x
+                                 ;; new x, insert first
+                                 (1+ (vget (1- k))))
+                           y (- x k))
+                     (loop while (and (< x n)
+                                      (< y m)
+                                      (char= (aref str1 x)
+                                             (aref str2 y)))
+                           do (incf x)
+                              (incf y))
+                     (vset k x)
+                     (format t "~%*** (d k x y) = (~A ~A ~A ~A) k-vec: ~A"
+                             d k x y k-vec)
+                     (progn
+                       (setf dkxy-list (delete-if #'(lambda (dkxy)
+                                                      (and (= d (first dkxy))
+                                                           (> x (third dkxy))))
+                                                  dkxy-list))
+                       (push (list d k x y) dkxy-list))
+                  when (and (>= x n) (>= y m))
+                    do (push dkxy-list dkxy-history)
+                       (return-from  myers-distance dkxy-history)
+                  finally (push dkxy-list dkxy-history))
             finally (return :fail)))))
+;; V1
+(defun diff-operations (str1 str2)
+  (let ((history (myers-distance str1 str2)))
+    (destructuring-bind (((prev-d prev-k prev-x prev-y)) &rest more-history)
+        history
+      (format t "~&111 ~A ~A ~A ~A" prev-d prev-k prev-x prev-y)
+      (cons (caar history)
+            (loop for dkxy-list in more-history
+                  as next-step = (if (null (cdr dkxy-list))
+                                     (first dkxy-list)
+                                     (find (1- prev-y) dkxy-list :test #'= :key #'fourth))
+                  do (format t "~&2222 ~A ~A ~A ~A ~A ~A" dkxy-list next-step prev-d prev-k prev-x prev-y)
+                     (setf prev-d (first next-step)
+                           prev-k (second next-step)
+                           prev-x (third next-step)
+                           prev-y (fourth next-step))
+                  collect next-step)))))
+
+;; V2
+(defun diff-operations (str1 str2)
+  (let ((history (myers-distance str1 str2)))
+    (destructuring-bind (((prev-d prev-k prev-x prev-y)) &rest more-history)
+        history
+      (format t "~&111 ~A ~A ~A ~A" prev-d prev-k prev-x prev-y)
+      (cons (caar history)
+            (loop for dkxy-list in more-history
+                  as next-step = (if (null (cdr dkxy-list))
+                                     (first dkxy-list)
+                                     (find (1- prev-y) dkxy-list :test #'= :key #'fourth))
+                  do (format t "~&2222 ~A ~A ~A ~A ~A ~A" dkxy-list next-step prev-d prev-k prev-x prev-y)
+                     (setf prev-d (first next-step)
+                           prev-k (second next-step)
+                           prev-x (third next-step)
+                           prev-y (fourth next-step))
+                  collect next-step)))))
 
 (defun backtrack (str1 str2)
   (let* ((backtrack-list (myers-distance str1 str2))
